ff797c05b5fc833de7c9a80b78e0725555d9387c
HemaSriBusi <hemasribusicse@gmail.com>
2019-06-05 23:43:00 +0530

    first commit

---
+++ compiler/compile.cpp
@@ -0,0 +1,12 @@
+// compile.cpp : Defines the entry point for the console application.
+//
+
+#define _CRT_SECURE_NO_WARNINGS
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
